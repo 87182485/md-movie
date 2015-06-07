5933ca870af2dd687331f5baa623483496a642d8
yfg0462@gmail.com <yfg0462@gmail.com>
2015-06-07 14:49:47 -0500

    create grid view on upcoming page

--- app/scripts/controllers/upcoming.js
+++ app/scripts/controllers/upcoming.js
@@ -6,6 +6,8 @@
 	function UpcomingCtrl($scope, upcomingService){
 		$scope.title = 'Upcoming';
 		
+		$scope.retrieveOriginalPosterUrl = retrieveOriginalPosterUrl;
+		
 		activate();
 		
 		function activate(){
@@ -14,10 +16,64 @@
 		
 		function getUpcoming(){
 			upcomingService.all().then(function(data){
-				console.log(data);
+				$scope.movies = wrapMovies(data.movies);
+				console.log($scope.movies);
 			}, function(err){
 				console.log(err);
 			})
+		}
+		
+		function wrapMovies(movies){
+			var mappedMovies = movies.map(function(item){
+				angular.extend(item, {
+					backgroundColor:backgroundGenerator(item.runtime),
+					gridAttribute:getGridValue(item.ratings.audience_score)
+				});
+				
+				return item;
+			});
+			
+			return mappedMovies;
+		}
+		
+		//utilize functions
+		//TODO: move those into a common service
+		function backgroundGenerator(movieLength){
+			if(movieLength>=120){
+				return "green";
+			}else if(movieLength<120&&movieLength>=100){
+				return "blue";
+			}else {
+				return "yellow";
+			}
+		}
+		
+		function getGridValue(score){
+			var rowNumber;
+			var columnNumber;
+			
+			if(score>=90){
+				rowNumber = 3;
+				columnNumber = 2;
+			}else if(score>=80){
+				rowNumber = 2;
+				columnNumber = 2;
+			}else if(score>=70){
+				rowNumber = 2;
+				columnNumber =1;
+			}else{
+				rowNumber = 1;
+				columnNumber = 1;
+			}
+			
+			return {
+				row:rowNumber,
+				col:columnNumber
+			};
+		}
+		
+		function retrieveOriginalPosterUrl(url){
+			return url.replace(/^.*?\/[\d]+x[\d]+\//,"http://");
 		}
 	}
 	
